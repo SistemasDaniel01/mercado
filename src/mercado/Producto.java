bf316249f43a27fb4df9059db8535804bf44538a
Daniel <danricas@gmail.com>
2022-05-16 10:47:17 -0500

    clase

---
+++ src/mercado/Producto.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mercado;
+
+/**
+ *
+ * @author planl
+ */
+public class Producto {
+    
+    private String nombre;
+    private int cantidad;
+
+    public Producto() {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+    
+    
+}
